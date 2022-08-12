e58d90c888bb43828ce97a2562ddf9cb46aa7d80
tomergriz <34768075+tomergriz@users.noreply.github.com>
2022-08-12 08:39:14 +0300

    starting point

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Heading } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <Heading as="h1" color="brand.color3">Hello world!</Heading>
+    <Heading as="h2">Foo</Heading>
+    </>
+)}
+
+export default App
---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+    brand: {
+        color1: "#F2F2F2",
+        color2: "#61788C",
+        color3: "#8C6F5E",
+        color4: "#D9B29C",
+        color5: "#402D26",
+    },
+    something : {
+        black: "#000000"
+    }
+};
+
+export default extendTheme({ colors });
